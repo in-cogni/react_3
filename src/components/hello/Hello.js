d1f60fac66e97ec9f598824ea488f299d80a8342
LadaKashubo <lkashubo@yandex.ru>
2025-08-30 21:37:39 +0300

    state

--- src/components/hello/Hello.js
+++ src/components/hello/Hello.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import './Hello.css';
 
-class Hello extends React.Component
-{
-    constructor(props)
-    {
+class Hello extends React.Component {
+    constructor(props) {
         super(props);
-        this.state = {text: "Hello"}
+        this.state = {
+            text: "Hello",
+            sign: "Rus"
+        }
     }
-    render()
-    {
-        return(
-            <div className="hello">{this.state.text}</div>
+    changeText = () => {
+        this.setState({ text: this.state.text === "Hello" ? "Привет" : "Hello" });
+        this.setState({sign: this.state.sign === "Rus" ? "Eng" : "Rus"})
+    }
+    render() {
+        return (
+            <div>
+                <button onClick={this.changeText}>{this.state.sign}</button>
+                <div className="hello">{this.state.text}</div>
+            </div>
         );
     }
 }
