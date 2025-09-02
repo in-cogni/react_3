fe3065b4b9014f9fcad3353f553f6ad5e87082c1
LadaKashubo <lkashubo@yandex.ru>
2025-09-02 21:38:18 +0300

    State day 2

--- src/components/App.js
+++ src/components/App.js
@@ -5,15 +5,33 @@ import Header from './header/Header.js';
 import Hello from './hello/Hello.js';
 import Length from './length/Length.js';
 import Convert from './convert/Convert.js';
+import Form from './form/Form.js';
+import Range from './range/Range.js';
+import Post from './post/Post.js';
+import Posts from './posts/Posts.js';
 
 class App extends React.Component {
+    state=
+    {
+        posts: 
+        [
+            {id:"1", title:"Процедурное программирование на языке c++", content:"Изучаются базовые конструкции языка c++"},
+            {id:"2", title:"Объектно-ориентированное программирование на языке c++", content:"Изучаются базовые концепции ООП"},
+            {id:"3", title:"DataContainers", content:"Изучаются основные структуры данных и контейнеры, основанные на них"}
+        ]
+    }
     render() {
         return (
             <div className="App">
-                <Header />
-                <Hello />
+                {<Header />
+                /*<Hello />
                 <Length/>
                 <Convert/>
+                <Form/>
+                <Range/> */}
+                {typeof(this.state.posts)}
+                <Posts posts={this.state.posts}/>
+                {/* <Post title="Title" content="Content"/> */}
             </div>
         );
     }
